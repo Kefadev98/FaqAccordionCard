1148dfdaee31b4af18771b6655d5df1c37605191
Stefan <stefankovacic4@gmail.com>
2022-07-19 13:04:09 +0200

    first commit

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const accItem = document.querySelectorAll('.faks-hidden');
-const accHeading = document.querySelectorAll('.faks-title');
+const accItem = document.querySelectorAll('.fax-hidden');
+const accHeading = document.querySelectorAll('.fax-title');
 
 for (i = 0; i < accHeading.length; i++) {
 
     accHeading[i].addEventListener('click', function () {
         let itemClass = this.parentNode.className;
         for (j = 0; j < accItem.length; j++) {
-            accItem[j].className = 'faks-hidden';
+            accItem[j].className = 'fax-hidden';
         }
-        if (itemClass == 'faks-hidden') {
+        if (itemClass == 'fax-hidden') {
             this.parentNode.className = 'selected';
         }
     })
